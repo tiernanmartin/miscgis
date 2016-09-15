51029fee8e42d6f227a2525b2cc36a0204125828
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-09-15 14:44:07 -0700

    built the package (no functions)

---
+++ R/miscgis-package.R
@@ -0,0 +1,8 @@
+#' Miscellaneous GIS Tools
+#'
+#' A collection of \R functions and RStudio Addins for GIS analysis.
+#'
+#' @name miscgis
+#' @docType package
+#' @import leaflet dplyr sp tigris
+NULL
