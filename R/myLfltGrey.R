7a6a9a375f4551b4b9024a40016506b1f0e93d57
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-09-19 14:32:57 -0700

    added parameters to control labels and "control" elements (e.g., zoom buttons, attribution line, etc.)

--- R/myLfltGrey.R
+++ R/myLfltGrey.R
@@ -2,11 +2,38 @@
 #'
 #' A subtle grayscale leaflet basemap to begin a leaflet pipechain with.
 #'
+#' @param labels Include OSM labels (styled by CartoDB) above all other layers (including paths).
+#' @param controls Show leaflet control elements (e.g., zoom buttons, attribution, etc.)
 #' @name myLfltGrey
 #' @import leaflet
 #' @export
 
-myLfltGrey <- function(){
+myLfltGrey <- function(labels = TRUE, controls = TRUE){
+
+        setup <- ifelse(labels|controls,
+                        "var myMap = this;",
+                        NULL)
+
+        lbl <- ifelse(labels,
+                      "var shadowPane = myMap.getPanes().shadowPane;
+                shadowPane.appendChild(
+                myMap.layerManager._byLayerId['tile\\nlabels_layer'].getContainer());",
+                      NULL
+        )
+
+        cntrl <- ifelse(controls,
+                        NULL,
+                        "myMap.removeControl(myMap.attributionControl);
+                                      myMap.removeControl(myMap.zoomControl);")
+
+        jqry <- paste0(
+                "function(el, t){",
+                setup,lbl,cntrl,"}"
+        )
+
         leaflet() %>%
-                addProviderTiles(provider = "Esri.WorldGrayCanvas")
+                addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
+                addProviderTiles(providers$CartoDB.PositronOnlyLabels,
+                                 layerId = 'labels_layer' ) %>%
+                htmlwidgets::onRender(jqry)
 }
