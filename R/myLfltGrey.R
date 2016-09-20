ec231a6d75043080e502d0c256207cf2c15780e3
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-09-19 17:11:40 -0700

    consolidated 'myLfltOpts' into 'myLfltGrey'

--- R/myLfltGrey.R
+++ R/myLfltGrey.R
@@ -2,18 +2,28 @@
 #'
 #' A subtle grayscale leaflet basemap to begin a leaflet pipechain with.
 #'
-#' @param labels Include OSM labels (styled by CartoDB) above all other layers (including paths).
-#' @param controls Show leaflet control elements (e.g., zoom buttons, attribution, etc.)
+#' @param bumpLabels Include OSM labels (styled by CartoDB) above all other layers (including paths).
+#' @param hideControls Show leaflet control elements (e.g., zoom buttons, attribution, etc.)
 #' @name myLfltGrey
 #' @import leaflet
 #' @export
 
 
-myLfltGrey <- function(){
+myLfltGrey <- function(bumpLabels = TRUE, hideControls = TRUE){
+        lbl <- if(bumpLabels == TRUE){"var shadowPane = myMap.getPanes().shadowPane;shadowPane.appendChild(myMap.layerManager._byLayerId['tile\\nlabels_layer'].getContainer());"}else{""}
+
+
+        cntrl <- if(hideControls == TRUE){"myMap.removeControl(myMap.attributionControl);myMap.removeControl(myMap.zoomControl);"}else{""}
+
+
+        jqry <- paste0("function(el, t){var myMap = this;",lbl,cntrl,"}")
+
+
         leaflet() %>%
                 addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
                 addProviderTiles(providers$CartoDB.PositronOnlyLabels,
-                                 layerId = 'labels_layer' )
+                                 layerId = 'labels_layer' ) %>%
+                htmlwidgets::onRender(jqry)
 
 }
 
