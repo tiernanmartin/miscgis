f4a960898af73326f59785991005cff3122a7c3e
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-09-20 20:46:14 -0700

    changed `miscgis_pals` from a named vector to a named list so that `leaflet` can access it.

--- R/miscgis_pals.R
+++ R/miscgis_pals.R
@@ -3,12 +3,18 @@
 #' A set of color palettes.
 #'
 # '@format A \code{list}.
-#' @import ggthemes RColorBrewer
+#' @import ggthemes RColorBrewer purrr dplyr
 #' @name miscgis_pals
 #' @export
 miscgis_pals <- {
         x <- list()
-        x$tableau_cat <- ggthemes::ggthemes_data$tableau$colors$tableau20[!grepl("light",names(ggthemes_data$tableau$colors$tableau20))]
+        library(purrr)
+        library(dplyr)
+        # cols_vctr <- ggthemes::ggthemes_data$tableau$colors$tableau20[!grepl("light",names(ggthemes_data$tableau$colors$tableau20))]
+
+        x$tableau_cat <-
+                ggthemes::ggthemes_data$tableau$colors[["tableau20"]] %>%
+                map(list) %>% subset(!grepl("light",names(.)))
 
         x
         }
