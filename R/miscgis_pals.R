be1a5de5e97bca6832959ebd14fd18e106dcdda8
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-09-20 20:53:42 -0700

    minor fix

--- R/miscgis_pals.R
+++ R/miscgis_pals.R
@@ -3,18 +3,16 @@
 #' A set of color palettes.
 #'
 # '@format A \code{list}.
-#' @import ggthemes RColorBrewer purrr dplyr
+#' @import ggthemes RColorBrewer
 #' @name miscgis_pals
 #' @export
 miscgis_pals <- {
+
         x <- list()
-        library(purrr)
-        library(dplyr)
-        # cols_vctr <- ggthemes::ggthemes_data$tableau$colors$tableau20[!grepl("light",names(ggthemes_data$tableau$colors$tableau20))]
 
         x$tableau_cat <-
                 ggthemes::ggthemes_data$tableau$colors[["tableau20"]] %>%
-                map(list) %>% subset(!grepl("light",names(.)))
+                purrr::map(list) %>% subset(!grepl("light",names(.)))
 
         x
         }
