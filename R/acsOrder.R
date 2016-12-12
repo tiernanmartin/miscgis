608d1b9047ff771746b150d1c1db601617c6cdb5
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-12-12 13:03:42 -0800

    added params to acsOrder() to differentiate between sorting and result columns

--- R/acsOrder.R
+++ R/acsOrder.R
@@ -4,8 +4,9 @@
 #'
 #' @param acs An \code{acs} object
 #' @param FUN A function for extracting information from an \code{acs} object (e.g. \code{estimate}, \code{standard.error})
-#' @param col The column to display, as a numeric index or as a character string
+#' @param order_col The column to order by, as a numeric index or as a character string
+#' @param result_col The column to display, as a numeric index or as a character string
 #' @import acs
 #' @export
 
-acsOrder <- function(acs,FUN = acs::standard.error,col = 1){acs[order(FUN(acs[,col])),col]}
+acsOrder <- function(acs,FUN = acs::standard.error,col = 1){acs[order(FUN(acs[,order_col])),result_col]}
