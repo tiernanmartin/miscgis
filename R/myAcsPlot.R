c4f9b2bc4bd41e4b982fdc703e2aa82f978fa3d8
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-12-12 10:47:33 -0800

    a sorted version of the acs dot plot

---
+++ R/myAcsPlot.R
@@ -0,0 +1,29 @@
+#' Ordered Dotplot with Error Bars
+#'
+#' An ordered dotplot with error bars and labels
+#'
+#' @param acs An \code{acs} object
+#' @param sort_fun A function for extracting information from an \code{acs} object (e.g. \code{estimate}, \code{standard.error}), which will be used to sort the \code{acs} object.
+#' @param col The column to display, as a numeric index or as a character string
+#' @param ylab = A label for the y-axis (should indicated the units, as in 'People of Color (%)')
+#' @param .pct = T/F, whether the units are a percentage or not
+#' @import acs
+#' @export
+
+myAcsPlot <- function(acs,sort_fun,col = 1,ylab,.pct = TRUE){
+        par(mar = c(1,4,1,1))
+
+        acs_obj <- acsOrder(acs,FUN = sort_fun,col = col)
+
+        acs.colnames(acs_obj) <- col
+
+        plot(acs_obj,ylab = ylab,yaxt = 'n',xaxt = 'n',bty = 'n')
+
+        yticks_val <- scales::pretty_breaks(n=5)(estimate(acs[,col]))
+
+        yticks <- scales::pretty_breaks(n=5)(estimate(acs[,col]))
+
+        y_labs <- {if(.pct){100*yticks}else{scales::comma(yticks)}}
+
+        axis(2, at=yticks_val,labels = y_labs)
+}
