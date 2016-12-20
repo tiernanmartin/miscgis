74d2ead20a3bc45a3302be17cb82a8d3536c2066
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-12-20 10:44:01 -0800

    added functions for setting breaks (useful for legends and axis labeling)

--- R/utils.R
+++ R/utils.R
@@ -64,7 +64,25 @@ is_pct <- function(x){
         if(!is.double(x)){FALSE}else if(max(x,na.rm = TRUE)<=1 & min(x, na.rm = TRUE) >=0){TRUE}else{FALSE}
         }
 
+#' @rdname utils
+#' @export
+label_pct <- function(breaks){
+        b <- breaks
+        b_max <- max(b) %>% scales::percent()
+        b_others <- b[(!(b %in% max(b)))]*100 %>% as.integer()
+        b_final <- c(b_others,b_max)
+        return(b_final)
+}
 
+#' @rdname utils
+#' @export
+label_yr <- function(breaks){
+        b <- breaks
+        b_min <- min(b)
+        b_others <- b[(!(b %in% b_min))] %>% str_sub(3,4) %>% paste0("'",.)
+        b_final <- c(b_min,b_others)
+        return(b_final)
+}
 injectHighlightHandler <- function() {
 
         code <- "
