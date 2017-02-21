23551a6b5dab98aa9862bcb714a9535ea87e0efc
Tiernan Martin <tiernan.martin@gmail.com>
2017-02-20 23:59:26 -0800

    changed the currency conversion function to a rate calculator (better for iterating over long lists)

--- R/currency_convert_rate.R
+++ R/currency_convert_rate.R
@@ -2,20 +2,19 @@
 #'
 #' A one-to-one spatial overlay function, similar to sp::over()
 #'
-#' @param x A dollar amount
 #' @param target_year The year to convert the dollar value to
 #' @param base_year The year to convert the dollar value to
-#' @name currency_convert
+#' @name currency_convert_rate
 #' @import acs
 #' @export
 
-currency_convert <- function(x,target_year,base_year){
+currency_convert_rate <- function(target_year,base_year){
         .env = environment()
         data("cpi", envir = .env)
         target_rate <-  cpi[as.character(target_year)]
         base_rate <-  cpi[as.character(base_year)]
         rate <-  target_rate/base_rate
-        x*rate[[1]]
+        rate[[1]]
 
 }
 
