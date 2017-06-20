dfbff0c377d258e78538ae6df00021d4fe97526b
Tiernan Martin <tiernan.martin@gmail.com>
2017-06-20 20:44:54 +0000

    added `make_id()` | version bump

---
+++ R/make_id.R
@@ -0,0 +1,24 @@
+#' Make a unique character string for anonymous record identification
+#'
+#' Make a unique character string for anonymous record identification
+#'
+#' @param rn number of records (e.g. \code{nrow(df)})
+#' @param string a vector of characters to form the identifier string
+#' @param .shuffle logical, whether to shuffle the ids or keep them in order
+#' @name make_id
+#' @import plyr purrr
+#' @export
+
+make_id <- function(rn, string = LETTERS, .shuffle = FALSE){
+        # browser()
+        times <- plyr::round_any(log(rn,length(string)),1,ceiling)
+
+        res <- map_chr(.x = purrr::cross_n(rep(list(string),times)),
+                       .f = ~ paste(rev(.x), collapse = ""))
+
+        return(res[1:rn])
+
+        if(.shuffle) return(res[sample(rn,replace = FALSE)])
+
+}
+
