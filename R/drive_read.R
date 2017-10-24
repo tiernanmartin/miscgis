3fb4d489b314ed5a07df3fd922b462d5100e2294
Tiernan Martin <tiernan.martin@gmail.com>
2017-10-24 20:00:50 +0000

    fixed drive_read

--- R/drive_read.R
+++ R/drive_read.R
@@ -1,44 +1,34 @@
-#' Read a file from Google Drive.
-#'
-#'
-#' @param dribble a `dribble` object containing the googledrive information.
+#' @title Read a file from Google Drive.
+#' @description Read a file from Google Drive.
+#' @name drive_read
+#' @param dr_id a Google Drive object id (use \code{\link[googledrive]{as_id}}).
 #' @param read_fun a function to read the object into memory.
 #' @param .unzip A logical scalar, does the file need to be decompressed?
+#' @param folder_name If the target is \code{.zip} file, what is the name of the folder within the file.
 #' @param ... Arguments to pass to the `read_fun` function.
-#'
 #' @return an object, format depends on which `read_fun` is provided.
+#' @import googledrive
+#' @importFrom magrittr "%>%"
 #' @export
+drive_read <- function (dr_id, read_fun, .unzip = FALSE, folder_name = NULL, ...)
+{
 
-drive_read <- function(dribble, read_fun, .unzip = FALSE, ...){
+  d <- drive_get(id = dr_id)
 
-        # Download the file
+  f <- tempfile()
 
-        f <- tempfile()
+  d %>% drive_download(path = f)
 
-        url <- paste0("https://docs.google.com/uc?export=download&id=",dribble[["id"]])
+  tmp_dir <- tempdir()
 
-        download.file(url,f)
+  if (.unzip) {
 
-        # Unzip (if necessary)
+    unzip(f, exdir = tmp_dir, overwrite = TRUE)
 
-        if(.unzip){
-
-                tmp_dir <- tempdir()
-
-                unzip(f,exdir = tmp_dir,overwrite = TRUE)
-
-                name_without_zip <- str_replace_all(dribble[["name"]], ".zip","")
-
-                tmp_dir_files <- list.files(tmp_dir)
-
-                name_with_fileformat <- tmp_dir_files[str_detect(tmp_dir_files,name_without_zip) & !str_detect(tmp_dir_files,".zip")]
-
-                fp <- paste(tmp_dir,name_with_fileformat, sep = "/")
-        }else{
-                fp <- f
-        }
-
-        # Read the file
-        read_fun(fp,...)
-
+    fp <- paste(dirname(tmp_dir),basename(tmp_dir), folder_name, sep = "/")
+  }
+  else {
+    fp <- f
+  }
+  read_fun(fp, ...)
 }
