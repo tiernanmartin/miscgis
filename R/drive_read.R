9aed232776eaf784c17bc5ced656f4a5e5788c29
Tiernan Martin <tiernan.martin@gmail.com>
2017-10-24 20:15:49 +0000

    fix drive_read for .zip files that don't contain a target folder, but do contain a target file

--- R/drive_read.R
+++ R/drive_read.R
@@ -4,13 +4,13 @@
 #' @param dr_id a Google Drive object id (use \code{\link[googledrive]{as_id}}).
 #' @param read_fun a function to read the object into memory.
 #' @param .unzip A logical scalar, does the file need to be decompressed?
-#' @param folder_name If the target is \code{.zip} file, what is the name of the folder within the file.
+#' @param target_name If the target is \code{.zip} file, what is the name of the folder or file within the compressed \code{.zip} file.
 #' @param ... Arguments to pass to the `read_fun` function.
 #' @return an object, format depends on which `read_fun` is provided.
 #' @import googledrive
 #' @importFrom magrittr "%>%"
 #' @export
-drive_read <- function (dr_id, read_fun, .unzip = FALSE, folder_name = NULL, ...)
+drive_read <- function (dr_id, read_fun, .unzip = FALSE, target_name = NULL, ...)
 {
 
   d <- drive_get(id = dr_id)
@@ -25,7 +25,7 @@ drive_read <- function (dr_id, read_fun, .unzip = FALSE, folder_name = NULL, ...
 
     unzip(f, exdir = tmp_dir, overwrite = TRUE)
 
-    fp <- paste(dirname(tmp_dir),basename(tmp_dir), folder_name, sep = "/")
+    fp <- paste(dirname(tmp_dir),basename(tmp_dir), target_name, sep = "/")
   }
   else {
     fp <- f
