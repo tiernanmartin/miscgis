ef686851f3dc4eef368ba7ec9ac000f7c44166a6
Tiernan Martin <tiernan.martin@gmail.com>
2018-01-15 14:39:58 -0800

    fix this old function documentation

---
+++ R/save_paged_sf.R
@@ -0,0 +1,30 @@
+#' @title Print sf objects with paged tables
+#' @description Print sf objects with paged table.
+#' @name print_paged_sf
+#' @param x A sf-class data object
+#' @param n Number of records to show in the printed table
+#' @import sf
+#' @import rmarkdown
+#' @export
+print_paged_sf <- function (x, n = ifelse(options("max.print")[[1]] == 99999,
+                                               10, options("max.print")[[1]]))
+{
+        # browser()
+        nf = length(x) - 1
+        app = paste("and", nf, ifelse(nf == 1, "field", "fields"))
+        if (any(!is.na(st_agr(x))))
+                app = paste0(app, "\n", "Attribute-geometry relationship: ",
+                             summarize_agr(x))
+        print(st_geometry(x), n = 0, what = "Simple feature collection with",
+              append = app)
+        if (n > 0) {
+                y <- x
+                if (nrow(y) > n) {
+                        cat(paste("First", n, "features:\n"))
+                        y <- x[1:n, , drop = FALSE]
+                }
+                rmarkdown::paged_table(y)
+        }
+        rmarkdown::paged_table(x)
+        # invisible(x)
+}
